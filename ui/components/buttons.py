6b9b5abd39136714281ca4c34d7ead183164d845
Stuart Sullivan <sullivanstaurt94@gmail.com>
2022-05-24 21:50:43 -0400

    Update Button library to add ButtonList and TextButton

--- ui/components/buttons.py
+++ ui/components/buttons.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import pygame
 from pygame import Color, Surface
+from pygame.font import Font
 from ui.components.components import Component
+from ui.components.containers import Container
 from ui.utilities import x_center, y_center
 
 
@@ -11,6 +13,7 @@ class Button(Component):
 
     def __init__(self) -> None:
         self._background_color = None
+        self._focus_color = None
         self._on_click = None
         self._in_focus = False
         self._x_margin = 0
@@ -19,6 +22,10 @@ class Button(Component):
 
     def set_background_color(self, color: Color) -> Button:
         self._background_color = color
+        return self
+
+    def set_focus_color(self, color: Color) -> Button:
+        self._focus_color = color
         return self
 
     def set_on_click(self, callback: function) -> Button:
@@ -38,7 +45,7 @@ class Button(Component):
     def on_render(self) -> Surface:
         surface = Surface((self._w, self._h), pygame.SRCALPHA)
         if self._in_focus:
-            surface.fill(Color(255, 255, 255, 125))
+            surface.fill(self._focus_color)
         # Build the button Image Surface
         inner_surface = Surface(
             (self._w - self._x_margin, self._h - self._y_margin))
@@ -51,3 +58,66 @@ class Button(Component):
     def on_update(self) -> None:
         if self._on_click is not None:
             self._on_click()
+
+
+class TextButton(Button):
+    """ Text Button: A button that has text """
+
+    def __init__(self) -> None:
+        self._text = ""
+        self._text_color = None
+        self._font = None
+        super().__init__()
+
+    def set_text(self, text: str) -> TextButton:
+        self._text = text
+        return self
+
+    def set_font(self, font: Font) -> TextButton:
+        self._font = font
+        return self
+
+    def set_text_color(self, color: Color) -> TextButton:
+        self._text_color = color
+        return self
+
+    def on_render(self) -> Surface:
+        surface = super(Button, self).on_render()
+        if self._text is not None and self._font is not None:
+            surfacec = self.text_font.render(
+                self._text, False, self._text_color, surfacec)
+        return surface
+
+
+class ButtonList(Container):
+    """ Button List, a container to manage """
+
+    def __init__(self) -> None:
+        self._current_button = None
+        self._in_focused = False
+        super().__init__()
+
+    def get_focus(self) -> bool:
+        return self._in_focused
+
+    def set_focus(self, focus: bool) -> None:
+        if self._current_button is None:
+            self._current_button = 0
+        self._in_focused = focus
+
+    def select_next_button(self, direction: int):
+        self._components[self._order[self._current_button]].set_focus(False)
+        if direction < 0:
+            self._current_button = min(0, self._current_button + direction)
+        if direction > 0:
+            self._current_button = max(
+                len(self._order) - 1, self._current_button + direction)
+        self._components[self._order[self._current_button]].set_focus(True)
+
+    def add_button(self, id: str, component: Component) -> ButtonList:
+        self.add_component(id, component)
+        return self
+
+    def remove_button(self, id: str) -> ButtonList:
+        self.remove_component(id)
+        return self
